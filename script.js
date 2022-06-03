5b064c34efcaf33db6562f933ad660b3db60a28c
Vitoriapaoli <veepaoli18@gmail.com>
2022-06-03 15:20:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function lampadaAcessa(){
+
+    let lampada = document.getElementById("lampada").src = "./src/images/acessa.png";
+
+}
+
+function lampadaApagada(){
+
+    let lampada = document.getElementById("lampada").src = "./src/images/desligada.png";
+
+}
+
+
+
+
+
